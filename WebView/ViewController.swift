ce2bd7a56eb4531385da2b49310b145dd382f677
izzuddin012 <izzuddin@bukalapak.com>
2019-01-17 09:16:44 +0700

    change into gigasel

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -12,64 +12,10 @@ import uiview_frame_helpers
 
 class ViewController: UIViewController {
 
-    private let homeURLString: String = "https://lintaspoi.cloudappsolution.com/"
+    private let homeURLString: String = "https://gigasel.cloudappsolution.com/webview/home"
     
-    fileprivate var webView: FullScreenWKWebView = FullScreenWKWebView()
-    fileprivate lazy var refreshButton: UIImageView = createRefreshButton()
-    fileprivate lazy var homeButton: UIImageView = createHomeButton()
-    fileprivate lazy var contactMeLabel: UILabel = UILabel()
+    fileprivate var webView: WKWebView = WKWebView()
     
-    private func createRefreshButton() -> UIImageView {
-        let imageView = UIImageView()
-        imageView.setW(48, andH: 48)
-        imageView.image = UIImage(named: "refresh_icon")
-        return imageView
-    }
-    
-    private func createHomeButton() -> UIImageView {
-        let imageView = UIImageView()
-        imageView.setW(48, andH: 48)
-        imageView.image = UIImage(named: "home_icon")
-        return imageView
-    }
-    
-    private func setupContactMeLabel() {
-        view.addSubview(contactMeLabel)
-        contactMeLabel.text = "Suggestion? Text Me 0811343434"
-        contactMeLabel.textColor = .black
-        contactMeLabel.font = UIFont(name: "HelveticaNeue", size: 12)!
-        contactMeLabel.backgroundColor = UIColor(
-            red: 255, green: 255, blue: 255, alpha: 0.5
-        )
-        contactMeLabel.textAlignment = .center
-        contactMeLabel.layer.cornerRadius = 8
-        contactMeLabel.clipsToBounds = true
-        contactMeLabel.setW(200, andH: 24)
-        contactMeLabel.insideTopEdge(by: 24)
-        contactMeLabel.insideLeftEdge(by: 24)
-    }
-    
-    private func setupRefreshButton() {
-        view.addSubview(refreshButton)
-        setRefreshButtonPosition()
-        let gestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(
-            target: self,
-            action: #selector(reloadCurrentURL)
-        )
-        refreshButton.addGestureRecognizer(gestureRecognizer)
-        refreshButton.isUserInteractionEnabled = true
-    }
-    
-    private func setupHomeButton() {
-        view.addSubview(homeButton)
-        setHomeButtonPosition()
-        let gestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(
-            target: self,
-            action: #selector(reloadHomeURL)
-        )
-        homeButton.addGestureRecognizer(gestureRecognizer)
-        homeButton.isUserInteractionEnabled = true
-    }
     
     private func setupWebView() {
         view.addSubview(webView)
@@ -79,26 +25,36 @@ class ViewController: UIViewController {
     
     private func setWebViewPosition() {
         webView.setSizeFrom(view.frame.size)
+        if #available(iOS 11.0, *) {
+            if let window: UIWindow = UIApplication.shared.keyWindow {
+                webView.setH(webView.frame.height - window.safeAreaInsets.bottom)
+            }
+        }
         webView.insideLeftEdge(by: 0)
         webView.insideTopEdge(by: 0)
     }
     
-    private func setHomeButtonPosition() {
-        homeButton.insideRightEdge(by: 36)
-        homeButton.insideBottomEdge(by: 24)
-    }
-    
-    private func setRefreshButtonPosition() {
-        refreshButton.insideRightEdge(by: 36)
-        refreshButton.outsideTopEdge(of: homeButton, by: 8)
+    private func setupNavigation() {
+        title = "GIGASEL"
+        
+        let barButton = UIBarButtonItem(
+            image: UIImage(named: "ic_back"),
+            style: UIBarButtonItem.Style.plain,
+            target: self,
+            action: #selector(didTapBack)
+        )
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.leftBarButtonItem = barButton
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupWebView()
-        setupHomeButton()
-        setupRefreshButton()
-        setupContactMeLabel()
+        setupNavigation()
+    }
+    
+    @objc func didTapBack() {
+        webView.goBack()
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -106,13 +62,9 @@ class ViewController: UIViewController {
         coordinator.animate(
             alongsideTransition: { _ in
                 self.setWebViewPosition()
-                self.setHomeButtonPosition()
-                self.setRefreshButtonPosition()
             },
             completion: { _ in
                 self.setWebViewPosition()
-                self.setHomeButtonPosition()
-                self.setRefreshButtonPosition()
             }
         )
     }
